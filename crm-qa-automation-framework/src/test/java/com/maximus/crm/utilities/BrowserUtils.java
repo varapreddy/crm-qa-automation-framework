621142dfb0a1031c5d1625eb189efabfb4e0a51c
200341 <200341@MDRMM-98NJPN2.MAXCORP.MAXIMUS>
2018-07-13 16:13:53 -0400

    Second Commit
    BrowserUtils and pom.xml added

---
+++ crm-qa-automation-framework/src/test/java/com/maximus/crm/utilities/BrowserUtils.java
@@ -0,0 +1,64 @@
+package com.maximus.crm.utilities;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BrowserUtils {
+
+	public static void scrollDown() {
+		
+	}
+	
+	public static List<String> getElementsText (WebElement element){
+	
+		return null; 
+	}
+
+	public static List<String> getElementsText (By locator){
+		
+		return null; 
+	}
+
+	public static WebElement waitForVisibility(WebElement element) {
+		return null;
+	}
+	
+	public static WebElement waitForVisibility(By locator) {
+		return null;
+	}
+	
+	public static WebElement waitForClickability(WebElement element) {
+		return null;
+	}
+	
+	public static WebElement waitForClickability(By locator) {
+		return null;
+	}
+	
+	public static WebElement fluentWait() {
+		return null;
+	}
+	
+	public static void waitForPageToLoad() {
+		
+	}
+	
+public static void waitFor() {
+		
+	}
+
+public static void switchToWindow() {
+	
+}
+
+public static void hover() {
+	
+}
+
+
+
+
+	
+}
